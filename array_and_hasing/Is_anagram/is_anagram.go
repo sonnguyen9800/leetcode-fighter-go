018a35b0b7d2939be74c95541e8f5cf635b770f2
Son Nguyen Hoang <sonnguyen9800@gmail.com>
2024-11-10 11:46:30 +0700

    Update

--- array_and_hasing/Is_anagram/is_anagram.go
+++ array_and_hasing/Is_anagram/is_anagram.go
@@ -5,32 +5,15 @@ func IsAnagram(first_str string, second_str string) bool {
 		return false
 	}
 
-	runeMap := make(map[rune]int)
+	runeMap_first, runMap_second := make(map[rune]int), make(map[rune]int)
 
-	var first_str_array = []rune(first_str)
-	for _, rune := range first_str_array {
-		value, exists := runeMap[rune]
-		if !exists {
-			value = 1
-			runeMap[rune] = value
-		} else {
-			value += 1
-			runeMap[rune] = value
-		}
+	for i := 0; i < len(first_str); i++ {
+		runeMap_first[rune(first_str[i])]++
+		runMap_second[rune(first_str[i])]++
 	}
-	second_str_array := []rune(second_str)
-	runmap_second_str := make(map[rune]int)
 
-	for _, rune := range second_str_array {
-		_, exists := runeMap[rune]
-		if !exists {
-			return false
-		}
-		runmap_second_str[rune] += 1
-
-	}
-	for rune, value := range runeMap {
-		value_2, exist := runmap_second_str[rune]
+	for currentRune, value := range runeMap_first {
+		value_2, exist := runMap_second[currentRune]
 		if !exist || value != value_2 {
 			return false
 		}
