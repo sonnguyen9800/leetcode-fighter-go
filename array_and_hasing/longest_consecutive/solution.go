d1856eb274da6f44c6b6d081265ffa195c54cd50
Son Nguyen Hoang <sonnguyen9800@gmail.com>
2025-01-13 16:50:53 +0700

    test1

--- array_and_hasing/longest_consecutive/solution.go
+++ array_and_hasing/longest_consecutive/solution.go
@@ -21,3 +21,106 @@ func longestConsecutive(nums []int) int {
 	}
 	return greatestConsCount
 }
+
+func longestConsecutiveOnePassB(nums []int) int {
+
+	hash := make(map[int]int)
+
+	greatestConsCount := 0
+	for _, num := range nums {
+		if hash[num] == 0 {
+			next := hash[num+1]
+			previous := hash[num-1]
+
+			if previous == 0 && next == 0 {
+				hash[num] = 1
+			}
+
+			if next != 0 && previous == 0 {
+				// if num is first number of a series, num has no "follower"
+
+				// This meant "next" is the prev first number
+				prev_series_length := hash[num+1] // this also MUST containt length of the consecutive
+
+				// this meant we can accessed
+
+				last_value_in_series := num + prev_series_length
+				hash[last_value_in_series] = hash[last_value_in_series] + 1
+				hash[num] = hash[last_value_in_series] // Also update "new" last index to the length of series
+			}
+
+			if previous != 0 && next == 0 {
+				// if num is new leading number of the series, with no "follower"
+				last_leading_value := hash[num-1] // this also MUST containt length of the consecutive
+				hash[num] = last_leading_value + 1
+				hash[num-last_leading_value] = last_leading_value + 1 // this supposed to be the "first" value in the series ("lower-bound")
+
+			}
+
+			if previous != 0 && next != 0 {
+				last_leading_value := hash[num-1]  // the leading of a 1st series (upper bound)
+				first_leading_value := hash[num+1] // the lower bound of a series
+
+				new_series_length := last_leading_value + first_leading_value + 1
+				hash[num] = hash[new_series_length]
+				hash[num-last_leading_value] = new_series_length
+				hash[num+first_leading_value] = new_series_length
+			}
+
+		}
+	}
+	for _, num := range hash {
+		if num > greatestConsCount {
+			greatestConsCount = num
+		}
+	}
+	return greatestConsCount
+}
+func longestConsecutiveOnePass(nums []int) int {
+
+	hash := make(map[int]int)
+
+	greatestConsCount := 0
+	consecutiveCount := 0
+	for _, num := range nums {
+		if hash[num] == 0 {
+			next := hash[num+1]
+			previous := hash[num-1]
+
+			if previous == 0 && next == 0 {
+				hash[num] = 1
+				consecutiveCount = hash[num]
+			}
+
+			if next != 0 && previous == 0 {
+				hash[num+1] = hash[num+1] + 1
+				consecutiveCount = hash[num+1]
+				hash[num] = hash[num+1]
+			}
+
+			if previous != 0 && next == 0 {
+				hash[num] = hash[num-1] + 1
+				consecutiveCount = hash[num]
+				hash[num-1] = hash[num]
+				hash[num-hash[num]+1] = hash[num]
+			}
+
+			if previous != 0 && next != 0 {
+				hash[num+1] = hash[num+1] + hash[num-1] + 1
+				hash[num] = hash[num+1]
+				hash[num-1] = hash[num+1]
+				if hash[num-hash[num]+1] != 0 {
+					hash[num-hash[num]+1] = hash[num]
+
+				}
+
+				consecutiveCount = hash[num+1]
+			}
+
+			if consecutiveCount > greatestConsCount {
+				greatestConsCount = consecutiveCount
+			}
+		}
+	}
+	return greatestConsCount
+}
